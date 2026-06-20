29555a59d4e2d861006af1d2611a9e07d520df6f
shivani <24wh1a6625@bvrithyderabad.edu.in>
2026-06-20 11:25:52 +0530

    commit3

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express=require('express');   
+const express = require('express');
 const app=express();
-const port=process.env.PORT || 6700;   
+const port=process.env.PORT ||6700;
 
 app.use(express.static("frontend"));
 app.listen(port,function(){
-    console.log("App running on http://localhost:"+port );
-}); 
+    console.log("App ruunning on http://localhost:"+port);
+});
